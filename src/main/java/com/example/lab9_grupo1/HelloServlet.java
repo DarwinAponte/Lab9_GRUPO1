9e794e3dde1f7ca4f589e214494fb24f9fdc8a1e
Darwin Aponte <darwin.aponte@pucp.edu.pe>
2021-11-22 22:32:48 -0500

    lab9 init

---
+++ src/main/java/com/example/lab9_grupo1/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.example.lab9_grupo1;
+
+import java.io.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+
+        RequestDispatcher view = request.getRequestDispatcher("/home1.jsp");
+        view.forward(request,response);
+    }
+
+    public void destroy() {
+    }
+}
