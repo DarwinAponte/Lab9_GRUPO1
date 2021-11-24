0ecdb253507d279025430a61b92e76077931e485
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2021-11-24 02:09:43 -0500

    pequeña modificacion

--- src/main/java/com/example/lab9_grupo1/Beans/Paises.java
+++ src/main/java/com/example/lab9_grupo1/Beans/Paises.java
@@ -1,10 +1,19 @@
 package com.example.lab9_grupo1.Beans;
 
 public class Paises {
+    private String idPais;
     private String nombre_pais;
     private String continente;
     private double poblacion;
     private double tamano_pais;
+
+    public String getIdPais() {
+        return idPais;
+    }
+
+    public void setIdPais(String idPais) {
+        this.idPais = idPais;
+    }
 
     public String getNombre_pais() {
         return nombre_pais;
